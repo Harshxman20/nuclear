232978e3a039d07335f2e2352861c851f9cf2132
Harshxman20 <deltaxkingop177@gmail.com>
2024-11-25 07:42:25 +0530

    Add files via upload

---
+++ nuclearx.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7929824597:AAF3Ej3fnX4WQLgY2pESNohrVo_3PaGl4Ag")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@NNUCLEAR_OP")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("7700702349")
+
+
+
+
+
+
+
+  
